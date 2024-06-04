bbaf0ae7f493bf8160dec765f2542d05c7ddd274
sonwe1e <sonwe1e@outlook.com>
2024-06-04 15:42:20 +0800

    baselinev7

--- nnunetv2/training/nnUNetTrainer/variants/network_architecture/MyNet.py
+++ nnunetv2/training/nnUNetTrainer/variants/network_architecture/MyNet.py
@@ -1,150 +1,146 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchinfo import summary
 
 
-class SKFusion(nn.Module):
-    def __init__(self, dim, height=2, reduction=4):
-        super(SKFusion, self).__init__()
-
-        self.height = height
-        d = max(int(dim / reduction), 8)
-
-        self.avg_pool = nn.AdaptiveAvgPool3d(1)
-        self.mlp = nn.Sequential(
-            nn.Conv3d(dim, d, 1, bias=False),
-            nn.LeakyReLU(),
-            nn.Conv3d(d, dim * height, 1, bias=False),
-        )
-
-        self.softmax = nn.Softmax(dim=1)
-
-    def forward(self, in_feats):
-        B, C, D, H, W = in_feats[0].shape
-
-        in_feats = torch.cat(in_feats, dim=1)
-        in_feats = in_feats.view(B, self.height, C, D, H, W)
-
-        feats_sum = torch.sum(in_feats, dim=1)
-        attn = self.mlp(self.avg_pool(feats_sum))
-        attn = self.softmax(attn.view(B, self.height, C, 1, 1, 1))
-
-        out = torch.sum(in_feats * attn, dim=1)
-        return out
-
-
-class MSCHead(nn.Module):
-    def __init__(self, in_channels, out_channels):
+class DefineConv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=[1, 1, 3], expand_rate=1):
         super().__init__()
-        self.conv1 = nn.Conv3d(in_channels, in_channels, kernel_size=1)
-        self.conv3 = nn.Conv3d(in_channels, in_channels, kernel_size=3, padding=1)
-        self.conv5 = nn.Conv3d(in_channels, in_channels, kernel_size=5, padding=2)
-        self.conv7 = nn.Conv3d(in_channels, in_channels, kernel_size=7, padding=3)
-        self.sk = SKFusion(in_channels, height=4)
-        self.conv = nn.Conv3d(in_channels * 5, out_channels, kernel_size=7, padding=3)
+        expand_rate = expand_rate
+        self.conv_list = nn.ModuleList()
+        for _, kernel in enumerate(kernel_size):
+            if _ == 0:
+                self.conv_list.append(
+                    nn.Sequential(
+                        nn.Conv3d(
+                            in_channels,
+                            out_channels * expand_rate,
+                            kernel,
+                            padding=kernel // 2,
+                            groups=1,
+                        ),
+                        nn.InstanceNorm3d(out_channels * expand_rate, affine=False),
+                    )
+                )
+            elif _ == len(kernel_size) - 1:
+                self.conv_list.append(
+                    nn.Sequential(
+                        nn.Conv3d(
+                            out_channels * expand_rate,
+                            out_channels,
+                            kernel,
+                            padding=kernel // 2,
+                            groups=1,
+                        ),
+                        nn.InstanceNorm3d(out_channels, affine=False),
+                    )
+                )
+            else:
+                self.conv_list.append(
+                    nn.Sequential(
+                        nn.Conv3d(
+                            out_channels * expand_rate,
+                            out_channels * expand_rate,
+                            kernel,
+                            padding=kernel // 2,
+                        ),
+                        nn.InstanceNorm3d(out_channels * expand_rate, affine=False),
+                    )
+                )
+        self.residual = in_channels == out_channels
+        self.act = nn.LeakyReLU(inplace=True)
+        for m in self.modules():
+            if isinstance(m, nn.Conv3d):
+                nn.init.trunc_normal_(m.weight, std=0.06)
+                nn.init.constant_(m.bias, 0)
 
     def forward(self, x):
-        x1 = self.conv1(x)
-        x3 = self.conv3(x1)
-        x5 = self.conv5(x1)
-        x7 = self.conv7(x1)
-        x_sk = self.sk([x1, x3, x5, x7])
-        x = self.conv(torch.cat([x1, x3, x5, x7, x_sk], dim=1))
+        res = x
+        for _, conv in enumerate(self.conv_list):
+            x = conv(x)
+            if _ == len(self.conv_list) - 1:
+                x += res if self.residual else x
+            x = self.act(x)
         return x
 
 
-class DoubleConv(nn.Module):
-    """(Conv3D -> IN -> LeakyReLU) * 2"""
-
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.double_conv = nn.Sequential(
-            nn.Conv3d(in_channels, out_channels, 1, 1, 0),
-            nn.InstanceNorm3d(out_channels),
-            nn.LeakyReLU(inplace=True),
-            nn.Conv3d(out_channels, out_channels, 3, 1, 1),
-            nn.InstanceNorm3d(out_channels),
-            nn.LeakyReLU(inplace=True),
-            nn.Conv3d(out_channels, out_channels, 1, 1, 0),
-            nn.InstanceNorm3d(out_channels),
-        )
-        self.residual = in_channels == out_channels
-        self.leaky_relu = nn.LeakyReLU(inplace=True)
-
-    def forward(self, x):
-        return (
-            self.leaky_relu(self.double_conv(x) + x)
-            if self.residual
-            else self.leaky_relu(self.double_conv(x))
-        )
-
-
 class Down(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, num_conv=2, conv=DefineConv):
         super().__init__()
-        self.encoder = nn.Sequential(
-            # nn.Conv3d(in_channels, in_channels, 2, 2),
-            nn.AvgPool3d(2),
-            DoubleConv(in_channels, out_channels),
-            # DoubleConv(out_channels, out_channels),
+        assert num_conv >= 1, "num_conv must be greater than or equal to 1"
+        self.downsample = nn.AvgPool3d(2)
+        self.extractor = nn.ModuleList(
+            [
+                (
+                    conv(in_channels, out_channels)
+                    if _ == 0
+                    else conv(out_channels, out_channels)
+                )
+                for _ in range(num_conv)
+            ]
         )
 
     def forward(self, x):
-        return self.encoder(x)
+        x = self.downsample(x)
+        for extractor in self.extractor:
+            x = extractor(x)
+        return x
 
 
 class Up(nn.Module):
+
     def __init__(
         self,
         low_channels,
         high_channels,
         out_channels,
-        fusion_mode="add",
+        num_conv=1,
+        conv=DefineConv,
+        fusion_mode="cat",
     ):
         super().__init__()
 
         self.fusion_mode = fusion_mode
-        self.up = nn.ConvTranspose3d(
-            low_channels, high_channels, kernel_size=2, stride=2
-        )
-        self.conv = (
-            DoubleConv(2 * high_channels, out_channels)
+        self.up = nn.ConvTranspose3d(low_channels, high_channels, 2, 2)
+        self.upsample = (
+            conv(2 * high_channels, out_channels)
             if fusion_mode == "cat"
-            else DoubleConv(high_channels, out_channels)
+            else conv(high_channels, out_channels)
         )
-        # self.conv2 = DoubleConv(out_channels, out_channels)
+        self.extractor = nn.ModuleList(
+            [conv(out_channels, out_channels) for _ in range(num_conv)]
+        )
 
-    def forward(self, x1, x2):
-        x1 = self.up(x1)
+    def forward(self, x_low, x_high):
+        x_low = self.up(x_low)
         if self.fusion_mode == "cat":
-            x = torch.cat([x2, x1], dim=1)
+            x = torch.cat([x_high, x_low], dim=1)
         else:
-            x = x2 + x1
-        return self.conv(x)
+            x = x_high + x_low
+        x = self.upsample(x)
+        for extractor in self.extractor:
+            x = extractor(x)
+        return x
+
+
+class Out(nn.Module):
+    def __init__(self, in_channels, num_classes):
+        super().__init__()
+        self.conv1 = nn.Conv3d(in_channels, num_classes, 1)
+
+    def forward(self, x):
+        return self.conv1(x)
 
 
 class MyNet(nn.Module):
-    """
-    MyNet
-
-    Args:
-        in_channels (int): 输入通道数。
-        n_classes (int): 类别数。
-        depth (int, optional): 编码器和解码器的深度。默认为 4。
-        head_channels (int, optional): 头部卷积层的通道数。默认为 16。
-        encoder_channels (list of int, optional): 编码器每一层的通道数。
-            列表长度必须等于 `depth`。默认为 [16, 32, 64, 128, 256]。
-
-    Raises:
-        AssertionError: 如果输入参数不满足条件。
-    """
-
     def __init__(
         self,
         in_channels,
         n_classes,
         depth=4,
-        encoder_channels=[32, 64, 128, 256, 320],
+        encoder_channels=[64, 64, 128, 256, 320],
+        conv=DefineConv,
         deep_supervision=False,
     ):
         super().__init__()
@@ -154,26 +150,36 @@ class MyNet(nn.Module):
         self.deep_supervision = deep_supervision
         assert len(encoder_channels) == depth + 1, "len(encoder_channels) != depth + 1"
 
-        self.conv = DoubleConv(in_channels, encoder_channels[0])
+        self.conv = DefineConv(in_channels, encoder_channels[0])
         self.encoders = nn.ModuleList()  # 使用 ModuleList 存储编码器层
         self.decoders = nn.ModuleList()  # 使用 ModuleList 存储解码器层
 
         # 创建编码器层
         for i in range(self.depth):
-            self.encoders.append(Down(encoder_channels[i], encoder_channels[i + 1]))
+            self.encoders.append(
+                Down(
+                    in_channels=encoder_channels[i],
+                    out_channels=encoder_channels[i + 1],
+                    conv=conv,
+                    num_conv=2,
+                )
+            )
 
         # 创建解码器层
         for i in range(self.depth):
             self.decoders.append(
                 Up(
-                    encoder_channels[self.depth - i],
-                    encoder_channels[self.depth - i - 1],
-                    encoder_channels[self.depth - i - 1],
+                    low_channels=encoder_channels[self.depth - i],
+                    high_channels=encoder_channels[self.depth - i - 1],
+                    out_channels=encoder_channels[self.depth - i - 1],
+                    conv=conv,
+                    num_conv=1,
+                    fusion_mode="add",
                 )
             )
         self.out = nn.ModuleList(
             [
-                nn.Conv3d(encoder_channels[depth - i - 1], n_classes, 1, 1, 0)
+                nn.Conv3d(encoder_channels[depth - i - 1], n_classes, 1)
                 for i in range(depth)
             ]
         )
@@ -198,9 +204,5 @@ class MyNet(nn.Module):
             return self.out[-1](decoder_features[-1])
 
 
-if __name__ == "__main__":
-    model = MyNet(1, 24).cuda(4)
-    x = torch.randn((2, 1, 128, 224, 224)).cuda(4)
-    y = model(x)
-    for _ in y:
-        print(_.shape)
+model = MyNet(1, 1, deep_supervision=False)
+summary(model, input_size=(2, 1, 128, 128, 128))
